77f366b3f48abb69ae7464e93035fa9ba8f8cb12
gongsi <linchuangqiong66@163.com>
2019-06-11 13:21:13 +0800

    框架搭建

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/page/common/Login'
+import Container from '@/container/Container'
+import Dashboard from '@/page/dashboard'
+import Article from '@/page/article'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/',
+      redirect: '/dashboard',
+      name: 'Container',
+      component: Container,
+      children: [
+        {path: 'dashboard', name: '首页', component: Dashboard,
+          children: [
+            {path: 'dashboard1', name: '首页1', component: Dashboard,},
+            {path: 'dashboard2', name: '首页2', component: Dashboard,
+              children: [
+                {path: 'dashboard21', name: '首页21', component: Dashboard,},
+                {path: 'dashboard22', name: '首页22', component: Dashboard, },
+              ] },
+          ]
+        },
+        {path: 'article', name: '文章', component: Article, },
+      ]
+    },
+  ]
+})
