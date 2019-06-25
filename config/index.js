f9143cb261f1b80c0b83417e2ab8c7157186d74c
gongsi <linchuangqiong66@163.com>
2019-06-25 15:31:25 +0800

    对跨域进行支持

--- config/index.js
+++ config/index.js
@@ -10,11 +10,18 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
+      '/api':{
+        target : 'http://localhost:8080',//接口地址
+        changeOrigin : true,//是否跨域
+        pathRewrite : {
+          '^/api' : ''//地址重写
+        }
+      }
     },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
